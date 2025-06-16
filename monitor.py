8147b00bc461c8eb44184914df5d721a9da668f9
sdcollectortoys <sdcollectortoys@gmail.com>
2025-06-15 20:09:17 -0700

    Add files via upload

--- monitor.py
+++ monitor.py
@@ -50,6 +50,7 @@ def main():
 
     try:
         chrome_options = Options()
+        chrome_options.binary_location = "/usr/bin/chromium"
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
